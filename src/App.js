523a040d4e6cf56c957b624e04310060ab32dc58
frisko-sposad <safiko@ya.ru>
2020-08-05 12:11:59 +0300

    init: start songbird task

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import style from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div>
+      Workaet
+    </div>
+  )
+}
+
+export default Header;

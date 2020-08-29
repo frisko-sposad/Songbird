971503182f63cdde12329b122eace8216ebb2e67
frisko-sposad <safiko@ya.ru>
2020-08-29 20:59:28 +0300

    feat: add ****** to random bird

--- src/components/Main/RandomBird/RandomBird.jsx
+++ src/components/Main/RandomBird/RandomBird.jsx
@@ -7,16 +7,19 @@ import BirdDate from '../birdsData';
 
 const RandomBird = (props) => {
   let birdImg;
+  let birdName;
   if (props.showBird === false) {
     birdImg = RandomBirdImg;
+    birdName = '******';
   } else {
     birdImg = BirdDate[props.round][props.correctAnswer].image;
+    birdName = BirdDate[props.round][props.correctAnswer].name;
   }
   return (
     <div className={style.RandomBird}>
       <img className={style.RandomBirdImg} src={birdImg} alt="" />
       <div className={style.RandomBirdContainer}>
-        <div className={style.RandomBirdName}>проигрыватель</div>
+        <div className={style.RandomBirdName}>{birdName}</div>
         <AudioPlayer
           className={style.AydioPlayer}
           autoPlayAfterSrcChange={false}
